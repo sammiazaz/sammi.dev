d87952cd38ce1e6d83d83eba762011cc52679ba6
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-29 20:59:11 +0530

    Add Back to Top button and improve mobile responsiveness

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ function handleSubmit(e) {
     e.target.reset();
   }, 3000);
 }
+
+// Back to Top functionality
+const backToTopBtn = document.getElementById('backToTop');
+const mainContent = document.querySelector('.main');
+
+mainContent.addEventListener('scroll', () => {
+  if (mainContent.scrollTop > 300) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  mainContent.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
